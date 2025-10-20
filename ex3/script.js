290bbc98439e7383c81edb8dc8f63e8779a07d4b
iamPMV259 <minhvu2592005@gmail.com>
2025-10-20 08:41:53 +0700

    complete

---
+++ ex3/script.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementById('toggleButton');
+const myParagraph = document.getElementById('myParagraph');
+
+toggleButton.addEventListener('click', () => {
+    paragraph.classList.toggle('hidden');
+});
